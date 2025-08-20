a5c80dbb2e751c43135188a52007954152ad8600
Rayan <66010947+Rayan1088@users.noreply.github.com>
2025-08-20 06:51:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,12 @@ def display_database_page(table_name, title, use_turso_db=False):
         st.info(f"No Data Found In [{table_name}] Table.") 
 
 def main_app(use_turso_db, streamlit):
-    st.title("Factory Production Line Multiprocessing Tracking System")
+    # Configure the page to use wide layout
+    st.set_page_config(
+    page_title="Factory Production Line Multiprocessing Tracking System",
+    page_icon="📊",
+    layout="wide",  # This makes the page use the full width
+    initial_sidebar_state="expanded")
 
     st.sidebar.markdown("<div style='font-size:34px; font-weight:bold; color:red;'>Select A Page</div>", unsafe_allow_html=True)
     page = st.sidebar.selectbox(label="Select A Page", options=["Video Processing","Database Records"], label_visibility="collapsed")
